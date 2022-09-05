a00e679283dc87fbbe6d0ba3c78c5deb07e11e8f
qoqbobb <kail853@naver.com>
2022-09-05 15:14:55 +0900

    uservo

---
+++ src/main/java/com/spring/domain/UsersVO.java
@@ -0,0 +1,21 @@
+package com.spring.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+@Data
+public class UsersVO {
+	private Long USER_NO;
+	private String USER_ID;
+	private String USER_PW;
+	private String USER_NICKNAME;
+	private String USER_PHONE;
+	private String USER_EMAIL;
+	private Date USER_REGDATE;
+	private String USER_POST;
+	private String USER_ADDR1;
+	private String USER_ADDR2;
+	
+	
+
+}
